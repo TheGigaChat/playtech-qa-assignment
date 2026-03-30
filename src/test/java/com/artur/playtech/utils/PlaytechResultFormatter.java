a17ee4bd761e7f7cfe924fdda0567357305f68fc
Artur Dzekunov <artur.dzekunov10@gmail.com>
2026-03-30 12:01:52 +0300

    feature: add formatter for result export

---
+++ src/test/java/com/artur/playtech/utils/PlaytechResultFormatter.java
@@ -0,0 +1,32 @@
+package com.artur.playtech.utils;
+
+import com.artur.playtech.model.PlaytechResult;
+
+public class PlaytechResultFormatter {
+
+    public static String format(PlaytechResult result) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Playtech QA Assignment Results\n");
+
+        sb.append("1. Teams\n");
+        sb.append("Count: ").append(result.teamCount()).append("\n");
+
+        for (String teamName : result.teamNames()) {
+            sb.append("- ").append(teamName).append("\n");
+        }
+
+        sb.append("\n");
+        sb.append("2. Research Areas\n");
+
+        for (String researchArea : result.researchAreas()) {
+            sb.append("- ").append(researchArea).append("\n");
+        }
+
+        sb.append("\n");
+        sb.append("3. Estonia Job Link (available from both Tallinn and Tartu)\n");
+        sb.append(result.tallinnTartuJobLink()).append("\n");
+
+        return sb.toString();
+    }
+}
